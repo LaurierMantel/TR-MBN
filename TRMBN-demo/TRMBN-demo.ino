1a1e21e19a9a3bf73fff4904ea1f5d291008ed2a
Nolan Dey <ndey96@gmail.com>
2017-06-21 09:22:57 -0400

    Continously update volume using control change

--- TRMBN-demo/TRMBN-demo.ino
+++ TRMBN-demo/TRMBN-demo.ino
@@ -14,8 +14,8 @@ Adafruit_BMP280 bmp; // I2C
 int channel = 1; // Defines the MIDI channel to send messages on (values from 1-16)
 int velocity = 100; // Defines the velocity that the note plays at (values from 0-127)
 int note = 70; // b flat on bass clef
-int minPressure = 98000;
-int maxPressure = 103000;
+int minPressure = 97500;
+int maxPressure = 101000;
 int velocityBuckets = 100;
 int normDivisor = (maxPressure - minPressure)/velocityBuckets;
 float pressure = -1.0;
@@ -24,13 +24,15 @@ const int buttonPin = 12;
 bool isSustainOn = false;
 Bounce pushbutton = Bounce(buttonPin, 10);  // 10 ms debounce
 byte previousState = HIGH;         // what state was the button last time
+int CC = 7;
+bool isNoteOn = false;
 
 void setup() {
   Serial.begin(9600);
   Serial.println(F("BMP280 test"));
   pinMode(buttonPin, INPUT_PULLUP);
   if (!bmp.begin()) {  
-    Serial.println(F("Could Ânot find a valid BMP280 sensor, check wiring!"));
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring!"));
     while (1);
   }
 }
@@ -39,18 +41,31 @@ void loop() {
     pressure = bmp.readPressure();
     Serial.print(F("Pressure = "));
     Serial.println(pressure);
-    int normP = (pressure - minPressure)/normDivisor + 10;
+    int normP = (pressure - minPressure)/normDivisor;
     Serial.print("normP: ");
     Serial.println(normP);
-    usbMIDI.sendNoteOff(note,0,channel);
-    if (normP > velocityBuckets) {
-      usbMIDI.sendNoteOn(note,velocityBuckets,channel);
-    } else if (normP > 0) {
-//      usbMIDI.sendNoteOff(note,0,channel);
-      usbMIDI.sendNoteOn(note,normP,channel);
-    } else if (normP <= 0) {
-//      usbMIDI.sendNoteOff(note,0,channel);   
+    Serial.print("isNoteOn: ");
+    Serial.println(isNoteOn);
+    if (isNoteOn) {
+      if (normP > velocityBuckets) {
+        usbMIDI.sendControlChange(CC,velocityBuckets,channel);
+      } else if (normP > 0) {
+        usbMIDI.sendControlChange(CC,normP,channel);
+      } else if (normP <= 0) {
+        usbMIDI.sendNoteOff(note,0,channel);
+        isNoteOn = false;
+      }
+    } else {
+      if (normP > velocityBuckets) {
+        usbMIDI.sendNoteOn(note,velocityBuckets,channel);
+        isNoteOn = true;
+      } else if (normP > 0) {
+        usbMIDI.sendNoteOn(note,normP,channel);
+        isNoteOn = true;
+      }
     }
+    
+
 //  if (pushbutton.update()) {
 //    if (pushbutton.fallingEdge()) {
 //      count = count + 1;
